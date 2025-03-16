63866aad9c7a787cc75b2a6484dd0aa641b8ddbf
Melvin Shieh <shieh.melvin@gmail.com>
2025-03-16 18:57:05 +0000

    first draft of 21Game

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -16,6 +16,8 @@ under 17, it must hit, otherwise if it is 17 or over, it must stay. The player
 with the highest hand value that is 21 or under wins. If multiple players / 
 dealer has the same value, they tie. 
 
+Will just design a game for 2 players for now, 1 human and 1 computer dealer.
+
 -- Nouns (objects)
 -> Verbs (methods, behaviors)
 -o Adjectives (variables, state)
@@ -23,59 +25,190 @@ dealer has the same value, they tie.
 Nouns/Verbs Extracted:
 - Game 
     -> Determine Winner
-    -> Win
-    -> Lose
-    -> Tie
+        o Win
+        o Lose
+        o Tie
+    -> Play
+    -> Display Welcome / Goodbye Message
 - Participants:
     - Dealer
     - Players
         -> Hit
         -> Stay
+        -> Bet (can make automatic on first go)
+        o Bust
+        o Score
 - Hand (Players have Hands)
-    -> Bust
-    -> 21
     -> Reveal
     o Value (what the cards in a hand total to)
 - Deck 
+    -> Deal 
+    -> Shuffle
 - Cards (A deck has Cards)
     o Suits
     o Rank
 """
 
+import random
+
 class Participants:
     def __init__(self):
+        self.hand = Hand()
+    
+    def hit(self, card):
+        """
+        Takes the top-most card from the deck -> Deck().deal()
+        places it in the participant's hand -> Hand().add_card()
+        """
+        self.hand.add_card(card)
+    
+    def is_busted(self):
+        return self.hand.get_value() > 21
+    
+    def score(self):
         pass
 
 class Dealer(Participants):
-    pass
+    def __init__(self):
+        super().__init__()
+        
+    def reveal(self):
+        print(f'Dealer is showing: {self.hand.cards[0]}')
 
 class Player(Participants):
     def __init__(self):
-        pass
-    
-    def hit(self):
-        pass
-    
-    def stay(self):
-        pass
-
-class Hand:
-    pass
+        super().__init__()
 
 class Deck:
     SUITS = ['D', 'H', 'C', 'S']
     RANKS = list(range(2, 11)) + ['J', 'Q', 'K', 'A']
-    HIDDEN_CARD = ('X', 'X')
     
     def __init__(self):
         self.shuffle_deck()
         
     def shuffle_deck(self):
         self.deck = [Card(rank, suit) for suit in Deck.SUITS for rank in Deck.RANKS]
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        if not self.deck:
+            self.shuffle_deck()
+        return self.deck.pop()
 
+class Hand:
+    def __init__(self):
+        self.cards = []
+    
+    def add_card(self, card):
+        self.cards.append(card)
+    
+    def get_value(self):
+        cards = [card.rank for card in self.cards]
+        value = 0
+        
+        for card in cards:
+            if card in ['J', 'Q', 'K']:
+                value += 10
+            elif card == 'A':
+                value += 11
+            else:
+                value += int(card)
+        
+        aces = cards.count('A')
+        while aces and value:
+            value -= 10
+            aces -= 1
+        
+        return value
+    
+    def __str__(self):
+        return f'{[card for card in self.cards]}'
+    
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        # self.value = 
     
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+    
+class TwentyOneGame:
+    def __init__(self):
+        self.dealer = Dealer()
+        self.player = Player()
+        self.deck = Deck()
+    
+    def deal_cards(self):
+        """
+        This function will deal 1 card to a specific player
+        """
+        # This can be simplified later: 
+        self.dealer.hand.add_card(self.deck.deal())
+        self.player.hand.add_card(self.deck.deal())
+        self.dealer.hand.add_card(self.deck.deal())
+        self.player.hand.add_card(self.deck.deal())
+        
+    def player_turn(self):
+        print(f'Player Hand: {self.player.hand.cards}')
+        self.dealer.reveal()
+        
+        while True:
+            action = input('Hit or Stay? (type h or s): ')
+            if action.casefold()[0] not in ['h', 's']:
+                print('Invalid choice, please type hit or stay: ')
+                action = input()
+            
+            if action == 'h':
+                new_card = self.deck.deal()
+                self.player.hit(new_card)
+            
+            if action == 's':
+                break
+        
+    def dealer_turn(self):
+        while self.dealer.hand.get_value() < 17:
+            new_card = self.deck.deal()
+            self.dealer.hit(new_card)
+            if self.dealer.is_busted():
+                break
+        
+    def display_result(self):
+        # Need to work in what happens if busted, automatic win for the other player
+        dealer_value = self.dealer.hand.get_value()
+        player_value = self.player.hand.get_value()
+        
+        print(f'Dealer Hand: {self.dealer.hand}')
+        print(f'Dealer Value: {dealer_value}')
+        print()
+        print(f'Player Hand: {self.player.hand}')
+        print(f'Dealer Value: {player_value}')
+        
+        if self.player.is_busted():
+            print('Player is busted')
+        elif self.dealer.is_busted():
+            print('Dealer is busted')
+            
+        if not self.player.is_busted() or self.dealer.is_busted():
+            if player_value > dealer_value:
+                print('Player Wins')
+            elif player_value < dealer_value:
+                print('Dealer Wins')
+            else:
+                print('It is a Tie')
+        
+    def display_welcome_message(self):
+        print('Welcome to 21!')
+        
+    def display_goodbye_message(self):
+        print('Thanks for playing!')
+        
+    def play(self):
+        self.display_welcome_message()
+        self.deal_cards()
+        self.player_turn()
+        self.dealer_turn()
+        self.display_result()
+        self.display_goodbye_message()
+        
+game = TwentyOneGame()
+game.play()

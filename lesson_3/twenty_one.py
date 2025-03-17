2eef353822b2a3e859304f08838ec8c7f1b1c0f5
Melvin Shieh <shieh.melvin@gmail.com>
2025-03-17 01:54:11 +0000

    added betting for 21

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -53,47 +53,25 @@ import random
 
 class Participants:
     def __init__(self):
-        self.hand = Hand()
+        self.reset()
     
     def hit(self, card):
-        """
-        Takes the top-most card from the deck -> Deck().deal()
-        places it in the participant's hand -> Hand().add_card()
-        """
         self.hand.add_card(card)
     
     def is_busted(self):
         return self.hand.get_value() > 21
     
-    def score(self):
-        pass
+    def reset(self):
+        self.hand = Hand()
 
 class Dealer(Participants):
     def __init__(self):
         super().__init__()
-        
-    def reveal(self):
-        print(f'Dealer is showing: {self.hand.cards[0]}')
 
 class Player(Participants):
     def __init__(self):
         super().__init__()
-
-class Deck:
-    SUITS = ['D', 'H', 'C', 'S']
-    RANKS = list(range(2, 11)) + ['J', 'Q', 'K', 'A']
-    
-    def __init__(self):
-        self.shuffle_deck()
-        
-    def shuffle_deck(self):
-        self.deck = [Card(rank, suit) for suit in Deck.SUITS for rank in Deck.RANKS]
-        random.shuffle(self.deck)
-        
-    def deal(self):
-        if not self.deck:
-            self.shuffle_deck()
-        return self.deck.pop()
+        self.bankroll = 500
 
 class Hand:
     def __init__(self):
@@ -115,21 +93,53 @@ class Hand:
                 value += int(card)
         
         aces = cards.count('A')
-        while aces and value:
+        while aces and value > 21:
             value -= 10
             aces -= 1
         
         return value
     
     def __str__(self):
-        return f'{[card for card in self.cards]}'
+        return f'{[str(card) for card in self.cards]}'
+
+class Deck:
+    SUITS = ['D', 'H', 'C', 'S']
+    RANKS = list(range(2, 11)) + ['J', 'Q', 'K', 'A']
+    
+    def __init__(self):
+        self.shuffle()
+        
+    def shuffle(self):
+        self.deck = [Card(rank, suit) for suit in Deck.SUITS for rank in Deck.RANKS]
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        if not self.deck:
+            self.shuffle()
+        return self.deck.pop()
+    
+    def deal_hidden(self):
+        if not self.deck:
+            self.shuffle()
+        card = self.deck.pop()
+        card.hide()
+        return card
     
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+        self._hidden = False
+    
+    def hide(self):
+        self._hidden = True
+    
+    def reveal(self):
+        self._hidden = False
     
     def __str__(self):
+        if self._hidden == True:
+            return 'X of X'
         return f'{self.rank} of {self.suit}'
     
 class TwentyOneGame:
@@ -137,77 +147,134 @@ class TwentyOneGame:
         self.dealer = Dealer()
         self.player = Player()
         self.deck = Deck()
+        self.winner = None
     
     def deal_cards(self):
-        """
-        This function will deal 1 card to a specific player
-        """
-        # This can be simplified later: 
-        self.dealer.hand.add_card(self.deck.deal())
+        self.dealer.hand.add_card(self.deck.deal_hidden())
         self.player.hand.add_card(self.deck.deal())
         self.dealer.hand.add_card(self.deck.deal())
         self.player.hand.add_card(self.deck.deal())
-        
+    
     def player_turn(self):
-        print(f'Player Hand: {self.player.hand.cards}')
-        self.dealer.reveal()
-        
         while True:
+            print()
+            print(f'Player Hand: {self.player.hand}')
+            print(f'Dealer Hand: {self.dealer.hand}')
+            
             action = input('Hit or Stay? (type h or s): ')
             if action.casefold()[0] not in ['h', 's']:
-                print('Invalid choice, please type hit or stay: ')
-                action = input()
+                action = input('Invalid choice, please type hit or stay: ')
             
-            if action == 'h':
+            if action.casefold()[0] == 'h':
                 new_card = self.deck.deal()
                 self.player.hit(new_card)
+            else:
+                break
             
-            if action == 's':
+            if self.player.is_busted():
+                print('Player is busted')
+                self.winner = self.dealer
                 break
         
     def dealer_turn(self):
         while self.dealer.hand.get_value() < 17:
             new_card = self.deck.deal()
             self.dealer.hit(new_card)
+            
             if self.dealer.is_busted():
+                print('Dealer is busted')
+                self.winner = self.player
                 break
-        
+    
     def display_result(self):
         # Need to work in what happens if busted, automatic win for the other player
+        self.dealer.hand.cards[0].reveal()
         dealer_value = self.dealer.hand.get_value()
         player_value = self.player.hand.get_value()
         
+        print()
         print(f'Dealer Hand: {self.dealer.hand}')
         print(f'Dealer Value: {dealer_value}')
-        print()
         print(f'Player Hand: {self.player.hand}')
-        print(f'Dealer Value: {player_value}')
-        
-        if self.player.is_busted():
-            print('Player is busted')
-        elif self.dealer.is_busted():
-            print('Dealer is busted')
+        print(f'Player Value: {player_value}')
             
-        if not self.player.is_busted() or self.dealer.is_busted():
+        if not self.player.is_busted() and not self.dealer.is_busted():
             if player_value > dealer_value:
                 print('Player Wins')
+                self.winner = self.player
             elif player_value < dealer_value:
                 print('Dealer Wins')
+                self.winner = self.dealer
             else:
                 print('It is a Tie')
-        
+    
+    def settle_bets(self):
+        if self.winner == self.player:
+            self.player.bankroll += self.bet
+        if self.winner == self.dealer:
+            self.player.bankroll -= self.bet
+    
     def display_welcome_message(self):
         print('Welcome to 21!')
         
     def display_goodbye_message(self):
         print('Thanks for playing!')
         
+    def set_bet(self):
+        max_bet = self.player.bankroll
+        print(f'Your bankroll is {max_bet}.')
+        
+        while True:
+            bet = input('How much would you like to bet? ')
+            try:
+                bet = int(bet)
+            except:
+                print('Please enter an integer')
+                    
+            if bet > 1 and bet <= max_bet:
+                self.bet = bet
+                break
+            
+            print(f'Please enter an integer between 1 and {max_bet}')
+    
+    def reset(self):
+        self.deck.shuffle()
+        self.player.reset()
+        self.dealer.reset()
+        self.bet = 0
+        self.winner = None
+    
+    def play_again(self):
+        again = input('Play Again? (y/n): ')
+        if again.casefold()[0] not in ['y', 'n']:
+            again = input('Invalid response, enter y/n: ')
+            
+        if again.casefold()[0] == 'n':
+            return False
+        return True
+    
     def play(self):
         self.display_welcome_message()
-        self.deal_cards()
-        self.player_turn()
-        self.dealer_turn()
-        self.display_result()
+        
+        while True:
+            self.reset()
+            self.set_bet()
+            self.deal_cards()
+            
+            self.player_turn()
+            if not self.player.is_busted():
+                self.dealer_turn()
+            
+            self.display_result()
+            self.settle_bets()
+            
+            if not self.play_again():
+                break
+            
+            if self.player.bankroll == 0:
+                print('You are out of money!')
+                break
+            
         self.display_goodbye_message()
         
 game = TwentyOneGame()
